b3e60c219d9a9c02dcdb3de1d544a29ee76034f7
sakshee-19 <jocund.isha@gmail.com>
2017-02-06 21:19:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,11 @@ app.get('/article-one', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
+app.get('/article-three', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
