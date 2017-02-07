f191954fbe08b0ab4df90a1af6efc9c3158a883a
sakshee-19 <jocund.isha@gmail.com>
2017-02-07 13:29:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -79,17 +79,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.send(CreateTemplate(article-one));
+app.get('/:articlename', function (req, res) {
+  res.send(CreateTemplate(articles[articlename]));
 });
 
-app.get('/article-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
 
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 
 app.get('/ui/style.css', function (req, res) {
