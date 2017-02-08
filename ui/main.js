16cae874cb86e9517f1043fb740f6e1f6e6fc642
sakshee-19 <jocund.isha@gmail.com>
2017-02-08 21:10:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
 console.log('Loaded!');
 var element=document.getElementById('main-text');
 element.innerHTML='new value';
+
+//move image
+
+var img=document.getElementById('img')
+img.onclick= function(){
+    img.style.marginleft='100px';
+};
