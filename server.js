be65ea39f2f88d9fdcad0bc9518b918d8871f8bc
sakshee-19 <jocund.isha@gmail.com>
2017-02-13 20:33:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var counter=0;
+app.get('/counter',function(req,res){
+  counter=counter+1;
+  res.send(counter.toString());
+});
+
+
+
+
 var articles={
     'article-one': {
         title:'Article One |sakshee jain',
@@ -100,11 +110,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-var counter=0;
-app.get('/counter',function(req,res){
-  counter=counter+1;
-  res.send(counter.toString());
-});
+
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
