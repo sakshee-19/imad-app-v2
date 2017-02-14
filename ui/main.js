ed0de56c989e7f2719543771707dbecc31897a4b
sakshee-19 <jocund.isha@gmail.com>
2017-02-14 13:06:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,8 +21,25 @@ img.onclick = function (){
 
 var button=document.getElementById('counter');
 var counter=0;
+
+
 button.onclick = function(){
-counter=counter+1;
-var span=document.getElementById('counts');
-span.innerHTML=counter.toString();
+
+
+var request=new XMLHttpRequest();
+
+request.onreadystatechange=function(){
+  if(request.readystate===XMLHttpRequest.DONE)
+  {
+      
+  }
+  if(request.readystate===200)
+  {
+        var counter=request.responseText();
+        var span=document.getElementById('counts');
+        span.innerHTML=counter.toString();   
+  }
+};
+request.open('GET','http://sakshee-19.imad.hasura-app.io/counter',true);
+request.send(null);
 };
