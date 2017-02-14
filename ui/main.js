ebd25d24b1050c65366eb45c3cadfe5427a07777
sakshee-19 <jocund.isha@gmail.com>
2017-02-14 23:19:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -60,6 +60,7 @@ submit.onclick = function(){
                 {
                         //capture the list and update
                       var names=request.responseText;
+                      names=JSON.parse(names);
                       var list='';
                       for(var i=0; i<names.length ; i++){
                           list += '<li>'+names[i]+'</li>';
