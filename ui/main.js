aff5730cdf103ce91f5958126cb318bd491bc686
sakshee-19 <jocund.isha@gmail.com>
2017-02-14 12:42:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,9 +18,9 @@ img.onclick = function (){
 
 
 
-var counter=0;
-var button=document.getElementById('counter');
 
+var button=document.getElementById('counter');
+var counter=0;
 button.onclick = function(){
 counter=counter+1;
 var span=document.getElementById('counts');
