ccb44583b67d8d54f96313a10a95896b43a8ae16
sakshee-19 <jocund.isha@gmail.com>
2017-02-28 13:59:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.get('/', function (req, res) {
 function hash(input,salt){
     //how to create hash
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return ["pbkdf2","10000",salt,hashed.toString('hex')];
+    return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$1');
 }
 
 
